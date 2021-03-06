b9e0a21b67598927e659a5b0d705e60ea804c183
Elsa Wainblum <elsawainblum@gmail.com>
2021-03-06 17:12:32 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Enemy{
+public:
+    virtual void attack(){
+        cout << "I am an enemy" << endl;
+    }
+};
+
+
+class Ninja: public Enemy{
+public:
+    void attack(){
+        cout << "I am a ninja, ninja chop! -" << endl;
+    }
+};
+
+class Monster: public Enemy{
+public:
+    void attack(){
+        cout << "monster must eat you! -" << endl;
+    }
+};
+
+int main()
+{
+    Ninja n;
+    Monster m;
+    Enemy *enemy1 = &n;
+    Enemy *enemy2 = &m;
+    enemy1->attack();
+    enemy2->attack();
+    return 0;
+}
